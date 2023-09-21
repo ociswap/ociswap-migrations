37998f989dc5320768484c12b5c637933e4d64d1
Florian Pieper <florian@pieper.ai>
2023-09-21 17:38:22 +0200

    Internal refactoring reorder old_token and new_token

--- tokens/src/lib.rs
+++ tokens/src/lib.rs
@@ -3,23 +3,23 @@ use scrypto::prelude::*;
 #[blueprint]
 mod token_migration {
     struct TokenMigration {
-        new_token: Vault,
         old_token: Vault,
+        new_token: Vault
     }
 
     impl TokenMigration {
         pub fn instantiate(old_address: ResourceAddress, new_token: Bucket) -> Global<TokenMigration> {
-            assert_ne!(old_address, new_token.resource_address(), "Old and new token address are not allowed to be equal.");
+            assert_ne!(old_address, new_token.resource_address(), "Old and new token addresses are not allowed to be equal.");
             if let Some(old_total_supply) = ResourceManager::from_address(old_address).total_supply() {
                 assert_eq!(
-                    new_token.amount(),
                     old_total_supply,
+                    new_token.amount(),
                     "New token amount needs to be equal to the total supply of the old token."
                 );
             }
             (Self {
-                new_token: Vault::with_bucket(new_token),
                 old_token: Vault::new(old_address),
+                new_token: Vault::with_bucket(new_token)
             })
                 .instantiate()
                 .prepare_to_globalize(OwnerRole::None)
